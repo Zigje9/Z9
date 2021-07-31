2b1210770fd3f2983c81031bff4365edf2e2afd6
jekoo <pjkwprn@gmail.com>
2021-07-31 15:29:39 +0900

    feat : set global style

--- z9/src/index.tsx
+++ z9/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './assets/styles/globalStyle';
+import { theme } from './assets/styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

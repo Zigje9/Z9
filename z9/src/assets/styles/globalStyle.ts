20cd5037f220b40f195908cf6974607d12cdb1f8
jekoo <pjkwprn@gmail.com>
2021-08-01 16:22:05 +0900

    feat : set global font

--- z9/src/assets/styles/globalStyle.ts
+++ z9/src/assets/styles/globalStyle.ts
@@ -1,14 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
+import DoHyeon from '../fonts/DoHyeon-Regular.ttf';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
+  @font-face {
+    font-family: 'Do Hyeon';
+    src: url(${DoHyeon});
+  }
   html,
   body {
     overflow: auto;
     height: 100%;
     padding: 0;
     margin: 0;
+    font-family: 'Do Hyeon', sans-serif;
   }
   * {
     box-sizing: border-box;

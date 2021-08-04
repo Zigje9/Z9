95d36e37b46eb6c6e6cedcbeee8f36f40c1b5369
jekoo <pjkwprn@gmail.com>
2021-08-04 21:27:12 +0900

    feat : apply highlight color

--- z9/src/assets/information.ts
+++ z9/src/assets/information.ts
@@ -9,4 +9,21 @@ export const home = {
 
 export const about = {
   title: ['🤔 Who Am I?', '💎 Career', '🌏 Experience'],
+  name: ['94.03.15 박제구'],
+  info: [
+    'JavaScript',
+    '와',
+    'React',
+    '에 관심이 많고,',
+    '사용자 중심의 서비스를 구현하는 ',
+    'Web FrontEnd',
+    '를 공부하고 있습니다.',
+    '공부한 내용을 정리하고 복습하기 위해',
+    '블로그',
+    '를 운영하고 있습니다.',
+    '여행',
+    '을 좋아하고, 사람들과',
+    '소통',
+    '하는 것을 좋아합니다.',
+  ],
 };
--- z9/src/components/about/whoAmI.tsx
+++ z9/src/components/about/whoAmI.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import mypicture from '../../assets/images/mypic.jpeg';
+import { Github } from '@styled-icons/bootstrap/Github';
+import { Book } from '@styled-icons/boxicons-regular/Book';
+import { Mail } from '@styled-icons/feather/Mail';
+import * as info from '../../assets/information';
 
 const Background = styled.div`
   display: flex;
@@ -14,7 +18,13 @@ const Background = styled.div`
 const WhoAmIContainer = styled.div`
   width: 85%;
   height: 75vh;
-  background-color: pink;
+  background: rgb(87, 82, 82);
+  background: linear-gradient(
+    96deg,
+    rgba(87, 82, 82, 1) 0%,
+    rgba(144, 142, 140, 1) 55%,
+    rgba(214, 214, 214, 1) 100%
+  );
   border-radius: 10px;
   display: flex;
   flex-direction: row;
@@ -28,6 +38,7 @@ const ProfileContainer = styled.div`
   width: 30%;
   height: 100%;
   background-color: white;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
   @media ${(props) => props.theme.mobile} {
@@ -71,11 +82,149 @@ const Name = styled.p`
 const InfoContainer = styled.div`
   width: 70%;
   height: 100%;
-  background-color: green;
   @media ${(props) => props.theme.mobile} {
     width: 100%;
     height: 60%;
   }
+`;
+
+const ContentBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 33%;
+`;
+
+const GitHubIcon = styled(Github)`
+  margin-left: 5%;
+  width: 60px;
+  color: purple;
+  @media ${(props) => props.theme.mobile} {
+    width: 35px;
+  }
+`;
+
+const BlogIcon = styled(Book)`
+  margin-left: 5%;
+  width: 60px;
+  color: greenyellow;
+  @media ${(props) => props.theme.mobile} {
+    width: 35px;
+  }
+`;
+
+const MailIcon = styled(Mail)`
+  margin-left: 5%;
+  width: 60px;
+  color: orange;
+  @media ${(props) => props.theme.mobile} {
+    width: 35px;
+  }
+`;
+
+const InfoTextBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InfoText = styled.p`
+  color: white;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const HighlightText1 = styled.p`
+  background: rgb(211, 214, 0);
+  background: linear-gradient(
+    96deg,
+    rgba(211, 214, 0, 1) 0%,
+    rgba(255, 255, 84, 1) 60%,
+    rgba(249, 255, 203, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const HighlightText2 = styled.p`
+  background: rgb(0, 35, 214);
+  background: linear-gradient(
+    96deg,
+    rgba(0, 35, 214, 1) 0%,
+    rgba(48, 164, 246, 1) 57%,
+    rgba(204, 234, 255, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const HighlightText3 = styled.p`
+  background: rgb(134, 8, 189);
+  background: linear-gradient(
+    96deg,
+    rgba(134, 8, 189, 1) 0%,
+    rgba(179, 95, 227, 1) 57%,
+    rgba(233, 204, 255, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const HighlightText4 = styled.p`
+  background: rgb(30, 184, 0);
+  background: linear-gradient(
+    96deg,
+    rgba(30, 184, 0, 1) 0%,
+    rgba(97, 238, 69, 1) 69%,
+    rgba(204, 255, 146, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const HighlightText5 = styled.p`
+  background: rgb(251, 86, 18);
+  background: linear-gradient(
+    96deg,
+    rgba(251, 86, 18, 1) 0%,
+    rgba(238, 159, 69, 1) 69%,
+    rgba(255, 198, 146, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.8rem;
+  line-height: 90%;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 0.7rem;
+  }
+`;
+
+const TextLine = styled.div`
+  display: flex;
 `;
 
 const WhoAmI: React.FC = () => {
@@ -85,10 +234,54 @@ const WhoAmI: React.FC = () => {
         <ProfileContainer>
           <MyPicture src={mypicture}></MyPicture>
           <NameBox>
-            <Name>94.03.15 박제구</Name>
+            <Name>{info.about.name[0]}</Name>
           </NameBox>
         </ProfileContainer>
-        <InfoContainer></InfoContainer>
+        <InfoContainer>
+          <ContentBox>
+            <GitHubIcon></GitHubIcon>
+            <InfoTextBox>
+              <TextLine>
+                <HighlightText1>{info.about.info[0]}</HighlightText1>
+                &nbsp;
+                <InfoText>{info.about.info[1]}</InfoText>
+                &nbsp;
+                <HighlightText2>{info.about.info[2]}</HighlightText2>
+                &nbsp;
+                <InfoText>{info.about.info[3]}</InfoText>
+              </TextLine>
+              <InfoText>{info.about.info[4]}</InfoText>
+              <TextLine>
+                <HighlightText3>{info.about.info[5]}</HighlightText3>
+                &nbsp;
+                <InfoText>{info.about.info[6]}</InfoText>
+              </TextLine>
+            </InfoTextBox>
+          </ContentBox>
+          <ContentBox>
+            <BlogIcon></BlogIcon>
+            <InfoTextBox>
+              <InfoText>{info.about.info[7]}</InfoText>
+              <TextLine>
+                <HighlightText4>{info.about.info[8]}</HighlightText4>
+                &nbsp;
+                <InfoText>{info.about.info[9]}</InfoText>
+              </TextLine>
+            </InfoTextBox>
+          </ContentBox>
+          <ContentBox>
+            <MailIcon></MailIcon>
+            <InfoTextBox>
+              <TextLine>
+                <HighlightText5>{info.about.info[10]}</HighlightText5>
+                <InfoText>{info.about.info[11]}</InfoText>
+                &nbsp; &nbsp;
+                <HighlightText5>{info.about.info[12]}</HighlightText5>
+                <InfoText>{info.about.info[13]}</InfoText>
+              </TextLine>
+            </InfoTextBox>
+          </ContentBox>
+        </InfoContainer>
       </WhoAmIContainer>
     </Background>
   );
